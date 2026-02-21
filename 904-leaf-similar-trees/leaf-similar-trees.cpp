37bc040040e04853a45af721234cb908e738ec5d
Vaibhav Mittal <vaibhavmittal55552@gmail.com>
2026-02-21 14:43:42 +0530

    Time: 3 ms (7.25%) | Memory: 15.3 MB (77.97%) - LeetSync

---
+++ 904-leaf-similar-trees/leaf-similar-trees.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public: 
+    void Leafvector(TreeNode* root,vector<int>& ans){
+        if(!root)return;
+        if(root->left==NULL && root->right==NULL)ans.push_back(root->val);
+        Leafvector(root->left,ans);
+        Leafvector(root->right,ans);
+    }
+    bool leafSimilar(TreeNode* root1, TreeNode* root2) {
+        vector<int>r1;
+        vector<int>r2;
+        Leafvector(root1,r1);
+        Leafvector(root2,r2);
+        if(r1.size()!=r2.size())return false;
+        for(int i =0;i<r1.size();i++){
+            if(r1[i]!=r2[i])return false;
+        }
+        return true;
+    }
+};
